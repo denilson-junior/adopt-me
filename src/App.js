489aee38bdfc976f3ddea7a976d71440ad2f0892
Denilson <denilsonsouzagomesjunior@gmail.com>
2021-06-14 22:48:24 -0300

    changing pure React to JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Biguin",
-      animal: "dog",
-      breed: "dobberman",
-    }),
-    React.createElement(Pet, {
-      name: "Garfield",
-      animal: "cat",
-      breed: "strepsios",
-    }),
-    React.createElement(Pet, {
-      name: "Pitaceros",
-      animal: "dog",
-      breed: "rotveiller",
-    }),
-  ]);
-};
+  return(
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Biguin" animal="dog" breed="dobberman" />
+      <Pet name="Garfield" animal="cat" breed="strepsios" />
+      <Pet name="Pitaceros" animal="dog" breed="rotveiller" />
+    </div>
+  )
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
